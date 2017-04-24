12e86670fe55e328581a1e9dfae805d5cdcd859e
Ilya Shusterman <ilya@allaspect.com>
2017-04-24 19:52:26 +0300

    added restframe work successfully , need to add react

--- djreact/api.py
+++ djreact/api.py
@@ -3,6 +3,12 @@ from rest_framework.generics import ListAPIView
 from .serializers import LineItemSerializer, EntitySerializer
 from .models import LineItem, Entity
 
+
 class LineItemApi(ListAPIView):
     queryset = LineItem.objects.all()
-    
+    serializer_class = LineItemSerializer
+
+
+class EntityApi(ListAPIView):
+    queryset = Entity.objects.all()
+    serializer_class = EntitySerializer
